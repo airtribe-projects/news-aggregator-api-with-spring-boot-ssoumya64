02fe2016f8e698f72d1c2917b36b64d90f738023
Soumya Ranjan Sahoo <ssahoo4862@gmail.com>
2024-11-09 21:13:01 +0530

    First commit as Configuration File

---
+++ src/test/java/com/airtribe/newsaggregator/app/NewsAggregatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtribe.newsaggregator.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
